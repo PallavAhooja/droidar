4ddc65d04926cc3f3ceaae8d5fdbc7c3b009309c
Pallav <pallav.ahooja@shuttl.com>
2016-08-22 16:03:00 +0530

    Separated project into a library

---
+++ droidar2/src/test/java/com/droidar2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.droidar2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit com.droidar2.tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
